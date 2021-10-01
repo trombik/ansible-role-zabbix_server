0eedacb35fab4ad2ab7d9ec08494f65fd65625c5
Tomoyuki Sakurai <y@trombik.org>
2021-10-01 08:54:03 +0700

    bugfix: add initial tests

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -3,46 +3,43 @@ require "serverspec"
 
 package = "zabbix_server"
 service = "zabbix_server"
-config  = "/etc/zabbix_server/zabbix_server.conf"
-user    = "zabbix_server"
-group   = "zabbix_server"
-ports   = [PORTS]
-log_dir = "/var/log/zabbix_server"
-db_dir  = "/var/lib/zabbix_server"
+user    = "zabbix"
+group   = "zabbix"
+ports   = [10051]
+conf_dir = "/etc/zabbix"
+default_user = "root"
+default_group = "root"
 
 case os[:family]
 when "freebsd"
-  config = "/usr/local/etc/zabbix_server.conf"
-  db_dir = "/var/db/zabbix_server"
+  package = "zabbix54-server"
+  conf_dir = "/usr/local/etc/zabbix54"
+  default_group = "wheel"
 end
+config  = "#{conf_dir}/zabbix_server.conf"
 
 describe package(package) do
   it { should be_installed }
 end
 
 describe file(config) do
+  it { should exist }
   it { should be_file }
-  its(:content) { should match Regexp.escape("zabbix_server") }
-end
-
-describe file(log_dir) do
-  it { should exist }
-  it { should be_mode 755 }
+  it { should be_mode 640 }
   it { should be_owned_by user }
   it { should be_grouped_into group }
-end
-
-describe file(db_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
+  its(:content) { should match Regexp.escape("Managed by ansible") }
 end
 
 case os[:family]
 when "freebsd"
   describe file("/etc/rc.conf.d/zabbix_server") do
     it { should be_file }
+    it { should exist }
+    it { should be_owned_by default_user }
+    it { should be_grouped_into default_group }
+    it { should be_mode 644 }
+    its(:content) { should match Regexp.escape("Managed by ansible") }
   end
 end
 
